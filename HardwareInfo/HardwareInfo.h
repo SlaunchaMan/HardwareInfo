2c8f0f48227f8e318d5b0376031e62f04700dd4b
Jeff Kelley <SlaunchaMan@gmail.com>
2017-11-02 16:35:06 -0400

    Initial Commit

---
+++ HardwareInfo/HardwareInfo.h
@@ -0,0 +1,19 @@
+//
+//  HardwareInfo.h
+//  HardwareInfo
+//
+//  Created by Jeff Kelley on 11/2/17.
+//  Copyright © 2017 Jeff Kelley. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for HardwareInfo.
+FOUNDATION_EXPORT double HardwareInfoVersionNumber;
+
+//! Project version string for HardwareInfo.
+FOUNDATION_EXPORT const unsigned char HardwareInfoVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <HardwareInfo/PublicHeader.h>
+
+
